6f73f1b53e139c3f4eb950dba73a92da53f27f9f
John Glatts <johnglatts1@hotmail.com>
2019-12-24 14:24:59 -0500

    Add files via upload

---
+++ LEDS/LEDGame/examples/ChaseLED/ChaseLED.ino
@@ -0,0 +1,13 @@
+#include <LEDGame.h>
+
+int btn = 4; // button connected to pin 4
+
+LEDGame game(btn);  
+
+void setup() {
+  pinMode(btn, INPUT_PULLUP);
+}
+
+void loop() {
+  game.playChaseGame();
+}
